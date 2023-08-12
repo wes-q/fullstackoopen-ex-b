9080e691dc6ba3140b52cc8fe5b895d85c5b51bb
Wes <piuswesley@gmail.com>
2023-08-12 18:15:49 +0800

    Fix gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        node: true,
+        es2021: true,
+    },
+    extends: "eslint:recommended",
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    rules: {
+        eqeqeq: "error",
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": ["error", "always"],
+        "arrow-spacing": ["error", { before: true, after: true }],
+        "no-console": 0,
+    },
+};
